0813d46115077b18fbd026f019b4d51f78ba4c32
kouwdg <210679325@qq.com>
2025-03-24 11:48:11 +0800

    添加用户安全认证前

--- big-make2-trigger/src/main/java/org/example/trigger/http/RaffleLogin.java
+++ big-make2-trigger/src/main/java/org/example/trigger/http/RaffleLogin.java
@@ -1,12 +1,21 @@
 package org.example.trigger.http;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.api.IRaffleLogin;
 import org.example.types.model.Response;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 /**
  * 登录接口
  */
+@Slf4j
+@RestController
+@CrossOrigin
+@RequestMapping("/api/v1/raffle/login")
 public class RaffleLogin implements IRaffleLogin {
     @Override
     public Response<Boolean> login() {
@@ -16,5 +25,11 @@ public class RaffleLogin implements IRaffleLogin {
     @Override
     public Response<Boolean> register() {
         return null;
+    }
+
+    //http://localhost:8091/api/v1/raffle/login/test1
+    @GetMapping("test1")
+    public String test(){
+        return "a";
     }
 }
